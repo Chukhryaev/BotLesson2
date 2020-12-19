1e37f291d7ff6d6c24f5630e9e6a012df257c1de
Alexey <alexey.chukhryaev@gmail.com>
2020-12-19 12:19:06 +0300

    Stage 3 - Editing channel post

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(config.TOKEN)
 
 @bot.channel_post_handler()
 def channel_post(message):
-	print(message)
+	bot.edit_message_text(f"{message.text}\n\nBy Alexey Chukhryaev", message.chat.id, message.message_id)
 
 
 bot.polling(none_stop=True)
