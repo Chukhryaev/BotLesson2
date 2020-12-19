9cf58a1e77c14f6c3ba01364c21cdfbd0d2ac157
Alexey <alexey.chukhryaev@gmail.com>
2020-12-19 13:29:39 +0300

    Stage 4 - Adding buttons for message and getting callback

--- bot.py
+++ bot.py
@@ -1,12 +1,23 @@
-import config
 import telebot
+from telebot import types
+
+import config
 
 bot = telebot.TeleBot(config.TOKEN)
 
 
 @bot.channel_post_handler()
 def channel_post(message):
-	bot.edit_message_text(f"{message.text}\n\nBy Alexey Chukhryaev", message.chat.id, message.message_id)
+    buttons = types.InlineKeyboardMarkup()
+    buttons.add(*[types.InlineKeyboardButton(text="👍 Like", callback_data=f"like"),
+                  types.InlineKeyboardButton(text="👎 Dislike", callback_data=f"dislike")])
+    bot.edit_message_text(f"{message.text}\n\nBy Alexey Chukhryaev", message.chat.id, message.message_id,
+                          reply_markup=buttons)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def query(call):
+    bot.answer_callback_query(call.id, text=f"You voted {call.data}")
 
 
 bot.polling(none_stop=True)
